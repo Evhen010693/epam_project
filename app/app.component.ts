f27b8a91514f6c3673ccb2c44b42e4437058fc16
Yevhenii <evhen_football_7@mail.ru>
2017-05-31 15:16:32 +0300

    change empty project to starter kit

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-import { CoursesComponents } from './courses/courses.components'
-
-@Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2+2 App</h1><courses></courses>',
-    directives: [CoursesComponents]
-})
-export class AppComponent { }
